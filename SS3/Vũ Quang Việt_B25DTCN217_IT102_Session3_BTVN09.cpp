a18ceb0783c544db4232630be85015022d55a180
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 15:18:31 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN09.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN09.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN09.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	float n=6;
-	float A= 1/((n-1)*n) + 1/(n*(n+1)) + 1/((n+1)*(n+2));
-	printf("A = %f", A);
+	float n;
+	printf("n=");
+	scanf("%f", &n);
+	float A = 1/((n-1)*n) + 1/(n*(n+1)) + 1/((n+1)*(n+2));
+	printf("A = %.2f", A);
 	return 0;
 }
+
