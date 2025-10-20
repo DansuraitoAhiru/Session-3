a545e5b3f9deb2b7f793ce5da8731e62765a91f6
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 13:51:40 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN03.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN03.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN03.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	float r=36;
+	float r;
+	printf("Ban kinh hinh tron r =");
+	scanf("%f", &r);
 	float P=2*M_PI*r;
 	float S=M_PI*pow(r,2);
 	printf("P = %.2f \n", P);
