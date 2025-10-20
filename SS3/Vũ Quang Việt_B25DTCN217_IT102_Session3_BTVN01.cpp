69209fee63eae31b6cc5d0c6269c79238c171186
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 13:31:52 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN01.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN01.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN01.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <stdio.h>
 int main(){
 	char name [50];
 	printf("Ten cua ban:");
 	scanf("%[^\n]", name);
-	printf("Ten cua ban: %s \n", name);
 	printf("Xin chào %s", name);
 	return 0;
 }
