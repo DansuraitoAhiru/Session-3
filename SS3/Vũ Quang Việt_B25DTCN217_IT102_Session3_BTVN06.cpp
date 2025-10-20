ad06ad83d2c2f3da30cd2588d5aecbf1f107eeff
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 14:54:36 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN06.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN06.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN06.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main (){
-	float d=3.6;
-	float h=6.9;
+	float d,h;
+	printf("d=");
+	scanf("%f", &d);
+	printf("h=");
+	scanf("%f", &h);
 	float S=d*h;
 	printf("S = %.2f", S);
 	return 0;
