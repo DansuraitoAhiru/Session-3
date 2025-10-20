e3812ac1cf4b35c7ac35f46f127e5de343647d0b
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 15:21:19 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN10.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN10.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN10.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int a=9;
-	int b=6;
+	unsigned a,b;
+	printf("a=");
+	scanf("%u", &a);
+	printf("b=");
+	scanf("%u", &b);
 	float S=sqrt(a+sqrt(b+1)) + sqrt(b+sqrt(pow(a,2) + pow(b,2)));
 	printf("S = %.2f", S);
 	return 0;
 }
+
