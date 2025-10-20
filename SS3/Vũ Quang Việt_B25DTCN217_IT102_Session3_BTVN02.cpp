13228cf120fe8aa8907a374414b1d3f2e718de7e
DansuraitoAhiru <vqviet2007@gmail.com>
2025-10-20 14:39:19 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN02.cpp

--- SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN02.cpp
+++ SS3/Vũ Quang Việt_B25DTCN217_IT102_Session3_BTVN02.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main(){ 
-	float C = 69;
+	float C;
+	printf("C=");
+	scanf("%f", &C);
 	float F = C * 9/5 + 32;
 	printf("F = %.1f", F);
 	return 0;
 }
+
